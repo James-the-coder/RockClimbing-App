c16c0ecdf02a871fc35139ac12d8a71f98876ee7
James-the-coder <57504679+James-the-coder@users.noreply.github.com>
2022-01-23 14:26:03 +0000

    Add files via upload
    configurations for using firebase

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { getFirestore } from "firebase/firestore";
+import {initializeApp} from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4Y2FWgLaGPAtahuzuPm182YGIK8QGoHs",
+  authDomain: "rock-climbing-app.firebaseapp.com",
+  projectId: "rock-climbing-app",
+  storageBucket: "rock-climbing-app.appspot.com",
+  messagingSenderId: "618600251243",
+  appId: "1:618600251243:web:2b5980c6278087bf10562e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+export const authentication = getAuth(app);
+
+export const fireStoreDb = getFirestore(app);
