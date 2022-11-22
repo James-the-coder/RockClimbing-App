b6588cc56627aabfd5474b11c47c8bdc548e7816
James-the-coder <57504679+James-the-coder@users.noreply.github.com>
2022-11-22 22:09:51 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,12 +9,12 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC4Y2FWgLaGPAtahuzuPm182YGIK8QGoHs",
-  authDomain: "rock-climbing-app.firebaseapp.com",
+  apiKey: "**********************",
+  authDomain: "*********************",
   projectId: "rock-climbing-app",
   storageBucket: "rock-climbing-app.appspot.com",
-  messagingSenderId: "618600251243",
-  appId: "1:618600251243:web:2b5980c6278087bf10562e",
+  messagingSenderId: "***********",
+  appId: "************************",
 };
 
 // Initialize Firebase
